db6432f19d77ae98e0f41e613242674bdae90b97
MonsterEner <clouderneering@gmail.com>
2023-10-23 11:00:17 +0900

    1st commit message.

---
+++ src/test/java/com/playdata/gitexam/GitexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.gitexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
